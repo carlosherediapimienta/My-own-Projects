a5262f8553f6968aaa234e9961c6be5ae4eee6c8
Carlos Heredia Pimienta <73475603+carlosherediapimienta@users.noreply.github.com>
2020-10-28 13:16:10 +0100

    Add files via upload

---
+++ Python/Helps/if_vs_loc.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 28 13:02:22 2020
+
+@author: Carlos Heredia Pimienta
+
+Explanation: Comparison IF with .loc
+
+"""
+import pandas as pd
+
+# Seleccionamos el set de datos
+df_train_if = pd.read_csv("train.csv")
+df_train_loc = pd.read_csv("train.csv")
+
+df_train_if['Title'] = df_train_if.Name.str.extract(' ([A-Za-z]+)\.', expand = False)
+df_train_loc['Title'] = df_train_loc.Name.str.extract(' ([A-Za-z]+)\.', expand = False)
+
+# Con el IF
+for i in range(len(df_train_if.Title)):
+    if df_train_if.Title[i] == 'Mlle':
+            df_train_if.Title[i] = 'Miss'
+    elif df_train_if.Title[i] =='Ms':
+            df_train_if.Title[i] = 'Miss'
+    elif df_train_if.Title[i] == 'Mme':
+            df_train_if.Title[i] = 'Mrs'
+    elif (df_train_if.Title[i] != 'Mrs') and (df_train_if.Title[i] != 'Miss') and \
+            (df_train_if.Title[i] != 'Mr') and (df_train_if.Title[i] != 'Master'):
+            df_train_if.Title[i] = 'Rare'
+        
+            
+# Con el .loc
+    df_train_loc.loc[df_train_loc['Title'] == 'Mlle', 'Title'] = 'Miss'
+    df_train_loc.loc[df_train_loc['Title'] == 'Ms', 'Title'] = 'Miss'
+    df_train_loc.loc[df_train_loc['Title'] == 'Mme', 'Title'] = 'Mrs'
+    df_train_loc.loc[(df_train_loc['Title'] != 'Mrs') & (df_train_loc['Title'] != 'Miss') &\
+          (df_train_loc['Title'] != 'Mr') & (df_train_loc['Title'] != 'Masters'), 'Title'] = 'Rare'
