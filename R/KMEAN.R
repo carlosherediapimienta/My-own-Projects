f66c79c38f841ee7b6d796f157fdf76250a46384
C. Heredia Pimienta <73475603+carlosherediapimienta@users.noreply.github.com>
2021-01-29 16:56:08 +0100

    Create KMEAN.R

---
+++ R/KMEAN.R
@@ -0,0 +1,34 @@
+# Author: Carlos Heredia Pimienta
+# Date: 30/04/20
+# Explanation: How to implement KMEAN in R
+
+library(dplyr)
+library(lubridate)
+
+apr14 <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-apr14.csv")
+may14 <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-may14.csv")
+jun14 <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-jun14.csv")
+jul14 <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-jul14.csv")
+aug14 <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-aug14.csv")
+sep14 <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-sep14.csv")
+
+data14 <- bind_rows(apr14, may14, jun14, jul14, aug14, sep14)
+
+# Separate or mutate the Date/Time columns
+data14$Date.Time <- mdy_hms(data14$Date.Time)
+data14$Year <- factor(year(data14$Date.Time))
+data14$Month <- factor(month(data14$Date.Time))
+data14$Day <- factor(day(data14$Date.Time))
+data14$Weekday <- factor(wday(data14$Date.Time))
+data14$Hour <- factor(hour(data14$Date.Time))
+data14$Minute <- factor(minute(data14$Date.Time))
+data14$Second <- factor(second(data14$Date.Time))
+
+set.seed(20)
+clusters <- kmeans(data14[,2:3], 5)
+
+# Save the cluster number in the dataset as column 'Borough'
+data14$Borough <- as.factor(clusters$cluster)
+
+# Inspect 'clusters'
+str(clusters)
